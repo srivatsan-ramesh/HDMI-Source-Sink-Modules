c5efd55407bf22261bfd7b03219c26ab9bf536e3
srivatsansoft <sriramesh4@gmail.com>
2016-05-20 13:34:15 +0530

    a completed primitive test for testing HDMI model

--- tests/test_hdmi_model.py
+++ tests/test_hdmi_model.py
@@ -1,17 +1,8 @@
-from myhdl import instance, Simulation, delay, always, Signal, ResetSignal
+from myhdl import instance, Simulation, Signal, ResetSignal
 
+from hdmi.interfaces import VideoInterface, AuxInterface, HDMIInterface
 from hdmi.models import HDMITxModel, HDMIRxModel
-
-
-def clock_driver(clock, d=1):
-
-    half_period = delay(d)
-
-    @always(half_period)
-    def drive_clock():
-        clock.next = not clock
-
-    return drive_clock
+from hdmi.utils import clock_driver
 
 
 def test_tmds_codec():
@@ -19,14 +10,30 @@ def test_tmds_codec():
     clock = Signal(bool(0))
     reset = ResetSignal(0, True, False)
 
-    hdmi_tx_model = HDMITxModel(clock, reset) # incomplete
-    hdmi_rx_model = HDMIRxModel(clock, reset)
+    video_interface_tx = VideoInterface(clock)
+    aux_interface_tx = AuxInterface(clock)
+    hdmi_interface_tx = HDMIInterface(clock)
+
+    video_interface_rx = VideoInterface(clock)
+    aux_interface_rx = AuxInterface(clock)
+    hdmi_interface_rx = hdmi_interface_tx
+
+    hdmi_tx_model = HDMITxModel(clock, reset, video_interface_tx, aux_interface_tx, hdmi_interface_tx)
+    hdmi_rx_model = HDMIRxModel(clock, reset, video_interface_rx, aux_interface_rx, hdmi_interface_rx)
     clk = clock_driver(clock)
     video_data = int('10101010', 2)
+    aux_data = (0, 1, 2)
 
     @instance
     def test():
-        pass
+        yield video_interface_tx.write_pixel(video_data)
+        yield video_interface_rx.read_pixel()
+        assert video_interface_rx.get_pixel() == video_data
+
+        yield aux_interface_tx.write_aux(*aux_data)
+        yield aux_interface_rx.read_aux()
+        assert aux_interface_rx.get_aux_data() == aux_data
+
     return test, clk, hdmi_tx_model.process, hdmi_rx_model.process()
 
 t = test_tmds_codec()
